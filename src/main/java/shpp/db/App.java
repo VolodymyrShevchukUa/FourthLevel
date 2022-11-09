4cbe6a08c71aebbc3ecd5b1b5bd2b7cfae3b1817
Vova Shevchuk <volodymyr.shevchuk.ua@gmail.com>
2022-11-09 13:35:45 +0200

    first comit

---
+++ src/main/java/shpp/db/App.java
@@ -0,0 +1,13 @@
+package shpp.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
